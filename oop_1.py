0361d33c5e183652ae9ca36f674c20c84a6666ee
ReVadim <revani@yandex.ru>
2020-12-27 23:24:25 +0300

    Implement the method for the grading of the lecturers

--- oop_1.py
+++ oop_1.py
@@ -7,6 +7,18 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rating_lecturer(self, lecturer, course, grade):
+        if 0 <= grade <= 10:
+            if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+                if course in lecturer.rating_grades:
+                    lecturer.rating_grades[course] += [grade]
+                else:
+                    lecturer.rating_grades[course] = [grade]
+            else:
+                return 'Error'
+        else:
+            return 'Wrong grade, only 0 to 10'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -17,10 +29,9 @@ class Mentor:
 
 class Lecturer(Mentor):
     ''' The child class from class Mentor '''
-
     def __init__(self):
         super().__init__(name, surname)
-        self.lecturer_rating_grades = []
+        self.rating_grades = {}
 
 
 class Reviewer(Mentor):
@@ -38,11 +49,12 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
